1743b6f6a3c4af42b7198857b95bf0fc5aa0a913
akacarlll <147193447+akacarlll@users.noreply.github.com>
2024-02-28 15:53:06 +0100

    Update Mail.py

--- Mail.py
+++ Mail.py
@@ -1,7 +1,13 @@
 import smtplib,ssl
 from email.message import EmailMessage
+
+#Premier essai bugué
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login('vintedbot079@gmail.com', 'rima atpl jxum yedp')
+"""server.sendmail(from_addr='vintedbot079@gmail.com',
+                to_addrs='carlfogue193@gmail.com',
+                subject= ": vinted bot \nCe message est un bot\nJespere que tu as bienre cu les produits")
+"""
 
 email_password = 'rima atpl jxum yedp'
 email_sender = 'vintedbot079@gmail.com'
@@ -16,6 +22,7 @@ Urgent
 em = EmailMessage()
 em['From'] = email_sender
 em['To'] = email_receiver
+em['Subject'] = subject
 em.set_content(body)
 context = ssl.create_default_context()
 
@@ -23,9 +30,5 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender,email_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
 
-"""server.sendmail(from_addr='vintedbot079@gmail.com',
-                to_addrs='carlfogue193@gmail.com',
- 
-                subject= ": vinted bot \nCe message est un bot\nJespere que tu as bienre cu les produits")
-"""
+
 
