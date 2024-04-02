915b5975175b33eb55d4f7d4e289ef8f01cfdba5
Carl-Dimitri Fogue <147193447+akacarlll@users.noreply.github.com>
2024-04-02 06:26:34 +0200

    Add files via upload

---
+++ Get_Last.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from dateparser import parse
+
+df= pd.read_csv("C:\\Users\\carlf\\OneDrive\\Bureau\\Technique de programmation\\BOT VINTED\\final_data.csv")
+def get_dernier(df):
+    df["Taille"] = df["Taille"].str.replace("Size information", "")
+    df["État"] = df["État"].str.replace("Condition information", "")
+    df["Localisation"] = df["Localisation"].str.replace("Localisation", "")
+    df['Vues'] = df["Vues"].str.extract(r'(\d+)')
+
+
+
+    df['Ajouté'] = df["Date d'ajout"].str.extract(r'(\d+) heures')
+    df['Ajouté']  = df['Ajouté'].fillna(100).astype(int)
+    df_sorted = df.sort_values(by='Ajouté', ascending=True)
+    df_sorted.head(50)
+
+    return df_sorted(3)
+
+
+
