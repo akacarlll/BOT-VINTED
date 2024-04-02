d4cfa14091e1e71f7df96d915e760eb7ed63e44a
Carl-Dimitri Fogue <147193447+akacarlll@users.noreply.github.com>
2024-04-02 06:42:41 +0200

    Update Mail.py

--- Mail.py
+++ Mail.py
@@ -1,6 +1,10 @@
 import smtplib,ssl
 from email.message import EmailMessage
+from Get_Last import get_dernier
+import pandas as pd
 
+df= pd.read_csv("C:\\Users\\carlf\\OneDrive\\Bureau\\Technique de programmation\\BOT VINTED\\final_data.csv")
+df = get_dernier(df)
 #Premier essai bugué
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login('vintedbot079@gmail.com', 'rima atpl jxum yedp')
@@ -13,9 +17,18 @@ email_password = 'rima atpl jxum yedp'
 email_sender = 'vintedbot079@gmail.com'
 email_receiver = 'carlfogue193@gmail.com'
 
+prix = df["Prix"].iloc[0]
+marque = df["Marque"].iloc[0]
+taille = df["Taille"].iloc[0]
+ajout = df["Date d'ajout"].iloc[0]
+
 subject = "test envoie mail bot"
 body = """Ouvre vite ce mail
 Urgent 
+\nPrix : {prix}
+\nMarque : {marque}
+\nTaille : {taille}
+\nAjout : {ajout}
 !!!!
 
 """
