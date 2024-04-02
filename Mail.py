98e4a7439a9bf65b1efb28ea512d0af00c8c7b4c
Carl-Dimitri Fogue <147193447+akacarlll@users.noreply.github.com>
2024-04-02 22:54:02 +0200

    Update Mail.py

--- Mail.py
+++ Mail.py
@@ -3,45 +3,45 @@ from email.message import EmailMessage
 from Get_Last import get_dernier
 import pandas as pd
 
-df= pd.read_csv("C:\\Users\\carlf\\OneDrive\\Bureau\\Technique de programmation\\BOT VINTED\\final_data.csv")
-df = get_dernier(df)
-#Premier essai bugué
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login('vintedbot079@gmail.com', 'rima atpl jxum yedp')
-"""server.sendmail(from_addr='vintedbot079@gmail.com',
-                to_addrs='carlfogue193@gmail.com',
-                subject= ": vinted bot \nCe message est un bot\nJespere que tu as bienre cu les produits")
-"""
 
-email_password = 'rima atpl jxum yedp'
-email_sender = 'vintedbot079@gmail.com'
-email_receiver = 'carlfogue193@gmail.com'
+def send_email(prix, marque, taille, ajout, sender_email, sender_password, receiver_email):
+    subject = "Le BOT a trouvé un nouvel article"
+    body = f"""Ouvre vite ce mail
+    Urgent 
+    \nPrix : {prix}
+    \nMarque : {marque}
+    \nTaille : {taille}
+    \nAjout : {ajout}
+    !!!!
+    """
 
-prix = df["Prix"].iloc[0]
-marque = df["Marque"].iloc[0]
-taille = df["Taille"].iloc[0]
-ajout = df["Date d'ajout"].iloc[0]
+    message = EmailMessage()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+    message.set_content(body)
 
-subject = "test envoie mail bot"
-body = """Ouvre vite ce mail
-Urgent 
-\nPrix : {prix}
-\nMarque : {marque}
-\nTaille : {taille}
-\nAjout : {ajout}
-!!!!
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+        server.login(sender_email, sender_password)
+        server.send_message(message)
+def main2():
+    df = pd.read_csv("C:\\Users\\carlf\\OneDrive\\Bureau\\Technique de programmation\\BOT VINTED\\final_data.csv")
+    df = get_dernier(df)
 
-"""
-em = EmailMessage()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['Subject'] = subject
-em.set_content(body)
-context = ssl.create_default_context()
+    email_password = 'rima atpl jxum yedp'
+    email_sender = 'vintedbot079@gmail.com'
+    email_receiver = 'carlfogue193@gmail.com'
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(email_sender,email_password)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    prix = df["Prix"].iloc[0]
+    marque = df["Marque"].iloc[0]
+    taille = df["Taille"].iloc[0]
+    ajout = df["Date d'ajout"].iloc[0]
 
+    send_email(prix, marque, taille, ajout, email_sender, email_password, email_receiver)
+
+# Appeler la fonction main si ce script est exécuté en tant que programme principal
+if __name__ == "__main__":
+    main2()
 
 
